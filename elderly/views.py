37211f105dad202c5fd0e0c907fd3b39235338c0
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-03-28 17:28:24 +0530

    update in dashboard authentication feature

--- elderly/views.py
+++ elderly/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
 from django.http import JsonResponse,HttpResponse
 from elderly.models import api_keys,doctor,hospitals
+from django.contrib.auth import authenticate,login
+from django.contrib.auth import logout as logouts
+from elderly.models import RegUser,doctor,patients,prescription
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     
@@ -21,13 +26,42 @@ def health_report(request):
     return render(request,"health_report.html")
 
 def sign(request):
+    if request.method=="POST":
+        name=request.POST.get("name")
+        mobile=request.POST.get('phone')
+        patName=request.POST.get("patname")
+        location=request.POST.get("location")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        user=User.objects.create_user(username=name,password=password,email=email)
+        user.save()
+
+        regObj=RegUser(name=name,mobile=mobile,location=location,email=email,patName=patName)
+        regObj.save()
+        return redirect("/login")
     return render(request,"sign.html")
 
-def login(request):
-    return render(request,"login.html")
 
-def video_call(request):
-    return render(request,"video_call.html")
+def logout(request):
+    logouts(request)
+    return redirect("/")
+
+def login1(request):
+
+    if request.method=="POST":
+        name=request.POST.get("name")
+        password=request.POST.get("password")
+        user=authenticate(request,username=name,password=password)
+        if user is not None:
+            login(request,user)
+            return redirect("/dashboard")
+        else:
+            return HttpResponse("Invalid username or password")
+
+    return render(request,"login.html")
+@login_required(login_url="/login")
+def dashboard(request):
+    return render(request,"dashboard.html")
 
 
 def doctor_1(request):
